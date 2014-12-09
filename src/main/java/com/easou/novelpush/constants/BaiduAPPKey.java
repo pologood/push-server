c600148d0a1fdc6ba3b489cf3d88a99ddae24ed4
tyes <xd195666916@163.com>
2014-12-09 15:42:20 +0800

    Update BaiduAPPKey.java

--- src/main/java/com/easou/novelpush/constants/BaiduAPPKey.java
+++ src/main/java/com/easou/novelpush/constants/BaiduAPPKey.java
@@ -11,12 +11,6 @@ package com.easou.novelpush.constants;
  */
 public enum BaiduAPPKey {
 
-	ANDROID_TEST("z7p4M4mGq4dEvGaPIMF3oM9I", "Wvmq3jz7OVItaey8XyrjMQ3og7ejAck9"), // android测试
-	ANDROID_ONLINE("SOniIoTofV3ezn5B1Vmwvgcd", "DQOu0usGCNlqHssjfyobFeSUYCWqlVof"), // //
-																					// android线上
-	// 注意: ios区分线上和测试是根据开发证书和发布证书来控制的
-	IOS_FREE("ScYDgNGNXDAG7i94ZEYvNC6n", "clTt9uAEki7UIocKgQCRh3iIbGhCqnp5"), // Ios免费版本测试和线上
-	IOS_PAID("lGs7pmUPcUXNNR6ahjuaiXgL", "65vF8SN34tkGiImtnT5aUtrGY2HXnaSB");// Ios收费版本测试和线上
 
 	private String apiKey;
 	private String secretKey;
